85a23cd66e2278db4f844c05fe3b547b55a3c345
Rob Moffat <rob@kite9.com>
2025-07-28 11:31:41 +0100

    Parameterized region

--- examples/complete/aws/main.tf
+++ examples/complete/aws/main.tf
@@ -2,8 +2,13 @@ provider "aws" {
   region = local.region
 }
 
+variable "region" {
+  description = "AWS region to deploy into"
+  type        = string
+  default     = "us-east-1" # optional fallback
+}
+
 locals {
-  region = "eu-west-1"
   name   = "complete-s3-bucket"
 }
 
