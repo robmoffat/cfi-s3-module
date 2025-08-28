4fb125ff5c3e0eb01f6e3d873d46667e0df5421e
Rob Moffat <rob@kite9.com>
2025-08-28 16:00:39 +0200

    fixed typo

--- remote/azure/postgres/main.tf
+++ remote/azure/postgres/main.tf
@@ -1,3 +1,3 @@
 module "test" {
-    source "https://github.com/Azure/terraform-azurerm-avm-res-dbforpostgresql-flexibleserver/blob/0.1.4/examples/database/main.tf"
+  source = "git::https://github.com/Azure/terraform-azurerm-avm-res-dbforpostgresql-flexibleserver.git//examples/database?ref=0.1.4"
 }
