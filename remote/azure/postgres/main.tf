883f0df6739d4208c27cef926ae1994087af3e9c
Rob Moffat <rob@kite9.com>
2025-08-28 16:43:02 +0200

    going back...

--- remote/azure/postgres/main.tf
+++ remote/azure/postgres/main.tf
@@ -1,17 +1,3 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0"
-    }
-  }
-}
-
-provider "azurerm" {
-  features {}
-}
-
-module "pg_flex" {
-  source  = "Azure/avm-res-dbforpostgresql-flexibleserver/azurerm"
-  version = "0.1.4"
+module "test" {
+  source = "git::https://github.com/Azure/terraform-azurerm-avm-res-dbforpostgresql-flexibleserver.git//examples/database?ref=0.1.4"
 }
