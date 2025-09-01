ac828b28642b72518a8c4084c5c999ba51987f99
Rob Moffat <rob@kite9.com>
2025-09-01 16:52:03 +0100

    Fixing urls

--- remote/aws/bedrock/main.tf
+++ remote/aws/bedrock/main.tf
@@ -1,3 +1,6 @@
 module "bedrock" {
-  source  = "git::https://github.com/aws-ia/terraform-aws-bedrock.git/examples/agent-only?ref=v0.0.29"
+  source  = "aws-ia/bedrock/aws"
+  version = "0.0.20"
+  foundation_model = "anthropic.claude-v2"
+  instruction = "You are an automotive assisant who can provide detailed information about cars to a customer."
 }
--- remote/aws/s3bucket/main.tf
+++ remote/aws/s3bucket/main.tf
@@ -1,3 +1,3 @@
 module "s3_bucket" {
-  source = "git:https://github.com/terraform-aws-modules/terraform-aws-s3-bucket.git/examples/complete?ref=v5.7.0"
+  source = "git:https://github.com/terraform-aws-modules/terraform-aws-s3-bucket.git//examples/complete?ref=v5.7.0"
 }
