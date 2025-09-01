d79094f11af4685bd19adf55af60828df468dd7c
Rob Moffat <rob@kite9.com>
2025-09-01 17:30:20 +0100

    "fixing" the tf

--- remote/aws/rds/main.tf
+++ remote/aws/rds/main.tf
@@ -1,4 +1,3 @@
 module "rds" {
-  source  = "git::https://github.com/terraform-aws-modules/terraform-aws-rds-aurora.git//examples/postgresql?ref=v9.15.0"
-  version = "0.0.7"
+  source = "git::https://github.com/terraform-aws-modules/terraform-aws-rds-aurora.git//examples/postgresql?ref=v9.15.0"
 }
