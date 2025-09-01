f2465241a1c1bc02d4fb0def63ba9bc662da0c41
Rob Moffat <rob@kite9.com>
2025-09-01 17:32:15 +0100

    fixing region issue

--- remote/aws/vpc/main.tf
+++ remote/aws/vpc/main.tf
@@ -1,3 +1,8 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+}
+
 module "vpc" {
   source  = "git::https://github.com/terraform-aws-modules/terraform-aws-vpc.git//examples/complete?ref=v6.0.1" 
 }
