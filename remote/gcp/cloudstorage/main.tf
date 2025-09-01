8bfc9783c24050062eb4170d2c314525026fa60f
Rob Moffat <rob@kite9.com>
2025-09-01 15:17:53 +0100

    added two gcp modules

---
+++ remote/gcp/cloudstorage/main.tf
@@ -0,0 +1,4 @@
+module "cloud_storage" {
+  source  = "terraform-google-modules/cloud-storage/google"
+  version = "~> 11.1.0"
+}
---
+++ remote/gcp/virtualnetwork/main.tf
@@ -0,0 +1,4 @@
+module "vpc" {
+  source  = "terraform-google-modules/network/google"
+  version = "~> 11.1.1"
+}
