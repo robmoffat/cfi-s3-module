97bb804c3360bdbc8d5ffd66aaef9c5ac696c235
Rob Moffat <rob@kite9.com>
2025-09-03 12:56:43 +0100

    fixed url

--- remote/aws/bedrock/main.tf
+++ remote/aws/bedrock/main.tf
@@ -1,3 +1,3 @@
 module "bedrock" {
-  source  = "git::https://github.com/aws-ia/terraform-aws-bedrock/.git//examples/agent-with-guardrails?ref=0.0.29"
+  source  = "git::https://github.com/aws-ia/terraform-aws-bedrock.git//examples/agent-with-guardrails?ref=0.0.29"
 }
