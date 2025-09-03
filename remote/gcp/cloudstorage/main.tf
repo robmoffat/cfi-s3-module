ade5861a4a7c09c6bcdfaf5c3fb12251f9c027c4
Rob Moffat <rob@kite9.com>
2025-09-03 13:53:25 +0100

    fixed url

--- remote/gcp/cloudstorage/main.tf
+++ remote/gcp/cloudstorage/main.tf
@@ -1,4 +1,4 @@
 module "cloud_storage" {
-  source  = "git::https://github.com/terraform-google-modules/terraform-google-cloud-storage.git/examples/simple_bucket?ref=v11.1.0"
+  source  = "git::https://github.com/terraform-google-modules/terraform-google-cloud-storage.git//examples/simple_bucket?ref=v11.1.0"
   project_id = "woven-precept-353210"
 }
