b00ebdf2c3bf34f6859f8371b698a6fc6735df26
Rob Moffat <rob@kite9.com>
2025-10-06 12:25:56 +0100

    parameterizing project id

--- remote/gcp/cloudstorage/main.tf
+++ remote/gcp/cloudstorage/main.tf
@@ -1,4 +1,8 @@
+variable "project_id" {
+  type = string
+}
+
 module "cloud_storage" {
   source  = "git::https://github.com/terraform-google-modules/terraform-google-cloud-storage.git//examples/simple_bucket?ref=v11.1.0"
-  project_id = "woven-precept-353210"
+  project_id = var.project_id
 }
